848501eef865d827283d7dacba9c056efdd6d4d7
Yugandhar Pitchika <yugandharp20@gmail.com>
2024-10-26 13:24:40 +1100

    apply state to navbar and sidebar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,16 +1,27 @@
 "use client"
 
+import { useAppDispatch, useAppSelector } from "@/redux"
+import { setIsSidebarCollapsed } from "@/state"
 import { Bell, Link, Menu, Settings, Sun } from "lucide-react"
 import React from "react"
 
 const Navbar = () => {
+  const dispatch = useAppDispatch()
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  )
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  }
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* LEFT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
